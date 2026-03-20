5b889263f8996c8e755fbfe169981f7d30605154
sssspring <qhadldhsp0221@gmail.com>
2026-03-20 18:16:02 +0900

    20260320-swiper

--- js/book.js
+++ js/book.js
@@ -1,47 +1,48 @@
+async function bookData() {
+    const params = new URLSearchParams({
+        target: "title",
+        query: "미움받을 용기",
+        size: 10
+    });
+    const url = `https://dapi.kakao.com/v3/search/book?${params}`;
 
-        async function bookData() {
-            const params = new URLSearchParams({
-                target: "title",
-                query: "미움받을 용기",
-                size: 10
-            });
-
-            try {
-                const response = await fetch(`https://dapi.kakao.com/v3/search/book?${params}`, {
-                    method: 'GET',
-                    headers: {
-                        Authorization: "KakaoAK b738156fd15ab734180589d817caa04b"
-                    }
-                });
-
-                if (!response.ok) {
-                    throw new Error(`HTTP 오류! 상태 코드: ${response.status}`);
-                }
-
-                const data = await response.json();
-                console.log(data);
-
-                // .box 요소 전체 선택
-                const boxElements = document.querySelectorAll(".swiper-slide");
-
-                // documents 데이터를 각 box에 대응하여 렌더링
-                boxElements.forEach((box, i) => {
-                    const doc = data.documents[i];
-
-                    if (!doc) return; // 데이터가 부족할 경우 생략
-
-                    // 요소 생성 및 추가
-                    box.innerHTML = `<img src="${data.documents[i].thumbnail}">
-                        <h3>${data.documents[i].title}</h3>
-                        <h6>${data.documents[i].authors}</h6>
-                        <p>${data.documents[i].contents.substring(0, 60)}</p>
-                        <button>click</button>
-                    `
-                });
-
-            } catch (error) {
-                console.log('에러발생', error);
+    try {
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                Authorization: "KakaoAK b738156fd15ab734180589d817caa04b"
             }
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP 오류! 상태 코드: ${response.status}`);
         }
 
-        bookData();
+        const data = await response.json();
+        console.log(data);
+
+        // .box 요소 전체 선택
+        const boxElements = document.querySelectorAll("#new .swiper-slide");
+        console.log(boxElements)
+
+        // documents 데이터를 각 box에 대응하여 렌더링
+        boxElements.forEach((box, i) => {
+            const doc = data.documents[i];
+
+            if (!doc) return; // 데이터가 부족할 경우 생략
+
+            // 요소 생성 및 추가
+            box.innerHTML = `<img src="${data.documents[i].thumbnail}">
+                    <h3>${data.documents[i].title}</h3>
+                    <h6>${data.documents[i].authors}</h6>
+                    <p>${data.documents[i].price}</p>
+                    <button>click</button>
+                    `
+        });
+
+    } catch (error) {
+        console.log('에러발생', error);
+    }
+}
+
+bookData();
--- js/swiper.js
+++ js/swiper.js
@@ -1,26 +1,31 @@
 // #slider swiper
-
-var swiper = new Swiper(".mySwiper", {
+var slider_swiper = new Swiper(".sliderSwiper", {
     navigation: {
-        nextEl: "#slider.swiper-button-next",
-        prevEl: "#slider.swiper-button-prev",
+        nextEl: "#slider .swiper-button-next",
+        prevEl: "#slider .swiper-button-prev",
     },
     pagination: {
         el: "#slider .swiper-pagination",
         clickable: true,
     },
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
 });
 
-    // # new swiper
-
-    var new_swiper = new Swiper("new Swiper",{
-        sliderPerView: 5,
-        spaceBeween:30,
-        pagination:{
-            el:'@new .swiper-pagination',clickable: true,
-        },
-        navigation: {
-        nextEl: "#slider.swiper-button-next",
-        prevEl: "#slider.swiper-button-prev",
-        },
-    });
+// #new swiper
+const swiper = new Swiper('.newSwiper', {
+    slidesPerView: 5,
+    spaceBetween: 20,
+    pagination: {
+        el: '#new .swiper-pagination',
+        clickable: true,
+        type: "fraction",
+    },
+    navigation: {
+        nextEl: "#new .swiper-button-next",
+        prevEl: "#new .swiper-button-prev",
+    },
+    slidesPerGroup: 5
+});
